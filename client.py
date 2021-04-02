8d65727c212d62407fbcc19548a62a9ac7b9ddb0
Dharun Anandayuvaraj <dharun99@gmail.com>
2021-04-02 16:42:01 -0500

    Test Commit

--- client.py
+++ client.py
@@ -6,21 +6,49 @@ Created on Fri Mar 26 20:54:56 2021
 @author: Dharun Anand
 """
 
+import json
 import socket
+import sys, getopt
 
-HOST = '127.0.0.1'
-PORT = 50001
 
+def get_cmdArgs():
+    argumentList = sys.argv[1:]
+    short_options = ""
+    long_options = ["id=", "revproc=", "pkt="]
+    arguments, values = getopt.getopt(argumentList, short_options, long_options)
+    for current_argument, current_value in arguments:
+        if current_argument in ("--id"):
+            srcid = current_value
+        elif current_argument in ("--revproc"):
+            revproc = current_value
+        elif current_argument in ("--pkt"):
+            pktName = current_value
+    
+    return [srcid, revproc, pktName]
+            
+def get_pktData(pktName):
+    with open(pktName) as f:
+        return (json.load(f))
+    
 
 if __name__ == "__main__":
-    try:
-        print("Starting Client")
+    try:        
+        cmdArgs = get_cmdArgs()
+        pktData = json.dumps(get_pktData(cmdArgs[2]))
+        
+        print("Starting Client %s",cmdArgs[0])
+        HOST = '127.0.0.1'
+        PORT = cmdArgs[1]
+        
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((HOST, PORT))
-            s.sendall(b'Hello, world')
-            data = s.recv(1024)
+            s.sendall(bytes(pktData,encoding="utf-8"))
+            
+            #USE THIS ON SERVER SIDE: received = received.decode("utf-8")
+            
+            rcvdData = s.recv(1024)
     
-        print('Received', repr(data))
+        print('Received', rcvdData.decode("utf-8"))
     except KeyboardInterrupt:
         pass
   
--- revproc.py
+++ revproc.py
@@ -5,3 +5,47 @@ Created on Fri Mar 26 20:54:56 2021
 
 @author: dharun
 """
+
+import json
+import socket
+import sys, getopt
+
+
+def get_cmdArgs():
+    argumentList = sys.argv[1:]
+    short_options = ""
+    long_options = ["port="]
+    arguments, values = getopt.getopt(argumentList, short_options, long_options)
+    for current_argument, current_value in arguments:
+        if current_argument in ("--port"):
+            port = current_value
+    
+    return [port]
+            
+
+if __name__ == "__main__":
+    try:        
+        cmdArgs = get_cmdArgs()
+        
+        print("Starting Revers Proxy Server at Port %s",cmdArgs[0])
+        HOST = '127.0.0.1'
+        PORT = cmdArgs[0]
+        
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((HOST, PORT))
+            s.listen()
+            while True:
+                conn, addr = s.accept()
+                with conn:
+                    print('Connected by', addr)
+                    while True:
+                         data = conn.recv(1024)
+                         if not data:
+                             break
+                         print(data.decode("utf-8"))
+                         conn.sendall(data)
+
+
+    except KeyboardInterrupt:
+        pass
+  
